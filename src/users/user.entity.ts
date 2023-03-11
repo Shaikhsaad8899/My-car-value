813a2eabbd167a57effe112d6107aaae13bb4c90
Shaikh Saad <101267786+Shaikhsaad8899@users.noreply.github.com>
2023-03-11 20:42:23 +0530

    Initial Commit User and Report
    Entitiies Services and Controllers added

---
+++ src/users/user.entity.ts
@@ -0,0 +1,9 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+}
