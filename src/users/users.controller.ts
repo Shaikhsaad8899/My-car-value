30f242a7ac343202526b7990185338ff03afa40d
Shaikh Saad <101267786+Shaikhsaad8899@users.noreply.github.com>
2023-03-14 00:51:56 +0530

    Added Multiple APIs
    Multiple APIs added for Posting, Updating and Removing user data.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Query,
+} from '@nestjs/common';
 import { CreateuserDto } from './dtos/create-user.dto';
+import { UpdateuserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
@@ -8,5 +18,21 @@ export class UsersController {
   createUser(@Body() body: CreateuserDto) {
     this.usersService.create(body.email, body.password);
     console.log(body);
+  }
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateuserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,7 +14,51 @@ export class UsersService {
     const result = await this.userRepository.query(
       `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`,
     );
-    return result[0];
+    return result;
   }
-  
+  findOne(id: number) {
+    const results = this.userRepository.query(
+      `SELECT * FROM Users WHERE id = '${id}'`,
+    );
+    console.log(results);
+    return results;
+  }
+  find(email: string) {
+    const results = this.userRepository.query(
+      `SELECT * FROM Users WHERE email = '${email}'`,
+    );
+    return results;
+  }
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.userRepository.query(
+      `SELECT * FROM Users WHERE id = ${id}`,
+    );
+    if (!user) {
+      throw new Error('User Not Found.');
+    }
+    const email = attrs.email ? `'${attrs.email}'` : 'email';
+    const password = attrs.password ? `'${attrs.password}'` : 'password';
+    const query = `UPDATE Users SET email = ${email}, password = ${password} WHERE id = ${id}`;
+    return this.userRepository.query(query);
+  }
+
+  async remove(id: number) {
+    const user = await this.userRepository.query(
+      `SELECT * FROM Users WHERE id = '${id}'`,
+    );
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.userRepository.query(`DELETE FROM Users WHERE id = ${id}`);
+  }
 }
+
+// find(email: string){
+//     return this.userRepository.query(`${email}`);
+// }
+//   async create(email: string, password: string): Promise<User> {
+//     const user = new User();
+//     user.email = email;
+//     user.password = password;
+//     return await this.userRepository.save(user);
+//   }
